2bea2a7bc1f9bde90a161df87b5aad60e3c67d45
CR0NYM3X <88253386+CR0NYM3X@users.noreply.github.com>
2025-04-29 14:02:39 -0700

    Create consulta básica MSSQL.ps1

---
+++ powershell basic/consulta básica MSSQL.ps1
@@ -0,0 +1,46 @@
+ # Solicitar las credenciales al usuario desde una ventana de windows esto evita exponer las credenciales 
+$credenciales = Get-Credential
+
+# Definir el servidor y la base de datos
+$servidor = "192.168.1.100"
+$puerto = "1433"
+$baseDeDatos = "master"
+
+# Crear la cadena de conexión
+$connectionString = "Server=$servidor,$puerto;Database=$baseDeDatos;User Id=$($credenciales.UserName);Password=$($credenciales.GetNetworkCredential().Password);"
+
+# Crear la consulta SQL
+$query = "SELECT @@VERSION"
+
+# Ejecutar la consulta SQL
+try {
+    # Crear una nueva conexión SQL
+    $connection = New-Object System.Data.SqlClient.SqlConnection
+    
+    # Establecer la cadena de conexión para la conexión SQL
+    $connection.ConnectionString = $connectionString
+    
+    # Abrir la conexión al servidor SQL
+    $connection.Open()
+
+    # Crear un comando SQL utilizando la conexión abierta
+    $command = $connection.CreateCommand()
+    
+    # Establecer el texto del comando como la consulta SQL definida anteriormente
+    $command.CommandText = $query
+    
+    # Ejecutar el comando y obtener un lector de datos (SqlDataReader) para leer los resultados
+    $reader = $command.ExecuteReader()
+
+    # Leer los resultados de la consulta
+    while ($reader.Read()) {
+        # Imprimir el primer campo de cada fila (que en este caso es la versión del servidor SQL)
+        Write-Output $reader[0]
+    }
+
+    # Cerrar la conexión al servidor SQL
+    $connection.Close()
+} catch {
+    # Capturar cualquier error que ocurra durante la ejecución de la consulta y imprimirlo
+    Write-Output "Error: $_"
+}
